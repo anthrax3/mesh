26aa506dea6e3f8c6bad282f1ce789aa581d7cdf
Sebastian <s.vogel@gentics.com>
2018-03-23 13:16:54 +0100

    Fix language parameter is ignored in GraphQL response

--- core/src/test/java/com/gentics/mesh/core/graphql/GraphQLNodeLanguageSearchEnpointTest.java
+++ core/src/test/java/com/gentics/mesh/core/graphql/GraphQLNodeLanguageSearchEnpointTest.java
@@ -1,59 +1,49 @@
 package com.gentics.mesh.core.graphql;
 
-import static com.gentics.mesh.assertj.MeshAssertions.assertThat;
-import static com.gentics.mesh.test.ClientHelper.call;
-import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 import com.gentics.mesh.FieldUtil;
-import com.gentics.mesh.core.rest.graphql.GraphQLResponse;
 import com.gentics.mesh.core.rest.node.NodeCreateRequest;
 import com.gentics.mesh.core.rest.schema.impl.SchemaReferenceImpl;
 import com.gentics.mesh.core.rest.user.NodeReference;
 import com.gentics.mesh.parameter.impl.NodeParametersImpl;
 import com.gentics.mesh.test.TestSize;
-import com.gentics.mesh.test.context.AbstractMeshTest;
 import com.gentics.mesh.test.context.MeshTestSetting;
-import com.syncleus.ferma.tx.Tx;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import io.vertx.core.json.JsonObject;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.gentics.mesh.test.ClientHelper.call;
+import static com.gentics.mesh.test.TestDataProvider.PROJECT_NAME;
 
 @MeshTestSetting(useElasticsearch = true, testSize = TestSize.FULL, startServer = true)
 @RunWith(Parameterized.class)
-public class GraphQLNodeScrollSearchEndpointTest extends AbstractGraphQLSearchEndpointTest {
-
-	@Parameters(name = "query={0}")
+public class GraphQLNodeLanguageSearchEnpointTest extends AbstractGraphQLSearchEndpointTest {
+	@Parameterized.Parameters(name = "query={0}")
 	public static List<String> paramData() {
-		List<String> testQueries = new ArrayList<>();
-		testQueries.add("node-elasticsearch-scroll-query");
-		return testQueries;
+		return Arrays.asList("node-elasticsearch-language-query");
 	}
 
-	public GraphQLNodeScrollSearchEndpointTest(String queryName) {
+	public GraphQLNodeLanguageSearchEnpointTest(String queryName) {
 		super(queryName);
 	}
 
 	@Before
 	public void createNodes() {
 		String parentNodeUuid = tx(() -> folder("2015").getUuid());
-		for (int i = 0; i < 100; i++) {
+		Arrays.asList("de", "en").forEach(lang -> {
 			NodeCreateRequest nodeCreateRequest = new NodeCreateRequest();
 			nodeCreateRequest.setParentNode(new NodeReference().setUuid(parentNodeUuid));
 			nodeCreateRequest.setSchema(new SchemaReferenceImpl().setName("content"));
-			nodeCreateRequest.setLanguage("en");
-			nodeCreateRequest.getFields().put("slug", FieldUtil.createStringField("test" + i));
+			nodeCreateRequest.setLanguage(lang);
+			nodeCreateRequest.getFields().put("slug", FieldUtil.createStringField("test" + lang));
 			nodeCreateRequest.getFields().put("teaser", FieldUtil.createStringField("some teaser"));
 			nodeCreateRequest.getFields().put("content", FieldUtil.createStringField("Blessed mealtime again!"));
-			call(() -> client().createNode(PROJECT_NAME, nodeCreateRequest, new NodeParametersImpl().setLanguages("en")));
-		}
+			call(() -> client().createNode(PROJECT_NAME, nodeCreateRequest, new NodeParametersImpl().setLanguages(lang)));
+		});
 	}
 
 	@Test
